f0ab497879887839e677a7c28aa0e315c3321e5b
Monirul <monirul429588@gmail.com>
2026-06-17 15:01:27 +0600

    Added Env File

--- app/db/database.py
+++ app/db/database.py
@@ -5,7 +5,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-load_dotenv()
+
+
+from pathlib import Path
+
+env_path = Path(__file__).resolve().parents[2] / ".env"
+load_dotenv(dotenv_path=env_path)
 
 DB_DRIVER = os.getenv("DB_DRIVER")
 DB_USER = os.getenv("DB_USER")
